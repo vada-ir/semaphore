1ca77c98db5bfb5391a5eb601e32663b5e0246da
fzerorubigd <fzerorubigd@gmail.com>
2015-06-01 12:48:52 +0430

    add test for resizable semaphore

--- semaphore_test.go
+++ semaphore_test.go
@@ -54,3 +54,84 @@ func TestUsage(t *testing.T) {
 
 	<-fun
 }
+
+func TestResizable(t *testing.T) {
+	r := NewResizableSemaphore(10)
+
+	r.Acquire(10)
+
+	//	var unlocked = false
+	var fun = make(chan struct{})
+	go func() {
+		r.Acquire(3)
+		fun <- struct{}{}
+	}()
+
+	r.Resize(14)
+	<-fun
+
+	assert.Equal(t, 14, r.PermitCount())
+	assert.Equal(t, true, r.Stable())
+	r.Resize(5)
+
+	assert.Equal(t, false, r.Stable())
+	r.Release(13)
+
+	assert.Equal(t, true, r.Stable())
+}
+
+func TestResizePanic(t *testing.T) {
+	r := NewResizableSemaphore(10)
+
+	r.Acquire(10)
+
+	var fun = make(chan struct{})
+
+	go func() {
+		fun <- struct{}{}
+		r.Acquire(10)
+		fun <- struct{}{}
+	}()
+
+	<-fun
+	r.Resize(5)
+	assert.Equal(t, false, r.Stable())
+
+	r.Release(5)
+	assert.Equal(t, false, r.Stable())
+
+	r.Release(10)
+
+	assert.Equal(t, false, r.Stable())
+	r.Release(1)
+
+	assert.Equal(t, true, r.Stable())
+	<-fun
+}
+
+func TestResizePanic2(t *testing.T) {
+	r := NewResizableSemaphore(10)
+
+	r.Acquire(10)
+
+	var fun = make(chan struct{})
+
+	go func() {
+		fun <- struct{}{}
+		assert.Equal(t, 10, r.TryAcquire(10, time.Second))
+		fun <- struct{}{}
+	}()
+
+	<-fun
+	r.Resize(5)
+	assert.Equal(t, false, r.Stable())
+
+	r.Release(5)
+	assert.Equal(t, false, r.Stable())
+	r.Release(10)
+	assert.Equal(t, false, r.Stable())
+	r.Release(1) // Now we are in less than permit size
+	assert.Equal(t, true, r.Stable())
+
+	<-fun
+}
